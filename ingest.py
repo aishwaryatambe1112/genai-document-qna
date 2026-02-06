69995dd7e6aedcb770528098c5ab59f34cba55bd
Aishwarya Tambe <tambeaishwarya11@gmail.com>
2026-02-06 23:39:22 +0530

    Update imports to use langchain_community package

--- ingest.py
+++ ingest.py
@@ -1,7 +1,8 @@
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+
 
 loader = PyPDFLoader("data/sample.pdf")
 documents = loader.load()
